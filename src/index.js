7787498631fae95a61adc458f5c222db4cb02208
Vadislav_Rybalko <vlad_rybalko@mail.ru>
2020-09-28 12:35:04 +0300

    Добавил поиск

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import "./styles/main.css";
 import { load } from "./services/api.servise"
 import { pageMovieComponent } from "./components/page.movie.component"
-// import {  } from "./components/"
+import { search } from './services/search.servise';
+
+
 
 document.querySelector('#item-actors').addEventListener('click', (event) => load(event, 'person'))
 document.querySelector('#item-movies').addEventListener('click', (event) => load(event, 'movie'))
+
+document.querySelector('#search').oninput = search
 
 document.querySelector('.post__cards').addEventListener('click', (event) => pageMovieComponent(event))
